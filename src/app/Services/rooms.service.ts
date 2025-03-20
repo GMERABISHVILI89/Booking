80c6e69800a9344702e8494347dd73737aa4a107
GMERABISHVILI89 <gmerabishvili32@gmail.com>
2025-03-20 20:30:28 +0400

    room

--- src/app/Services/rooms.service.ts
+++ src/app/Services/rooms.service.ts
@@ -44,9 +44,27 @@ GetRoomsByHotelId(id: number):  Observable<ServiceResponse<Rooms[]>> {
   }
 
 
-addRoom(roomData: FormData): Observable<ServiceResponse<Rooms>> {
-  return this.http.post<ServiceResponse<Rooms>>(`${this.API_URL}/add-room`, roomData);
-}
+  addRoom(roomData: any, selectedImages: File[]): Observable<ServiceResponse<Rooms>> {
+    const formData = new FormData();
+  
+    // Append basic room details
+    formData.append('hotelId', String(roomData.hotelId));
+    formData.append('name', String(roomData.name));
+    formData.append('pricePerNight', String(roomData.pricePerNight));
+    formData.append('maximumGuests', String(roomData.maximumGuests));
+    formData.append('roomTypeId', String(roomData.roomTypeId));
+  
+    // Append room images
+    selectedImages.forEach((image, index) => {
+      formData.append(`roomImages`, image, image.name); // roomImages[] will be processed as a list
+    });
+  
+    // Debugging: Log FormData
+  
+  
+    // Make API request
+    return this.http.post<ServiceResponse<Rooms>>(`${this.API_URL}/add-room`, formData);
+  }
 
  
 }
